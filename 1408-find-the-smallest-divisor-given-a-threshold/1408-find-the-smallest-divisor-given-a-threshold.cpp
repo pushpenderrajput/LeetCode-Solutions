6e534510db9a03e649030a5e7770817a935962d9
Pushpender Rajput <85430197+pushpenderrajput@users.noreply.github.com>
2024-08-24 10:52:03 +0530

    Time: 44 ms (5.96%), Space: 24.8 MB (74.2%) - LeetHub

--- 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.cpp
+++ 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
-bool ispossible(vector<int>& nums, int threshold, int div){
-     int sum = 0;
-     for(auto i:nums){
-        sum +=ceil(i/(double)div);
+    bool Possible(vector<int>& nums, int threshold, int mid){
+        int dividor = 0;
+        for(int num: nums){
 
-     }
-     return sum <= threshold;
-}
-    int smallestDivisor(vector<int>& nums, int threshold) {
-        int l =1, r= *max_element(nums.begin(),nums.end()) , ans = 0;
-        while(l<=r){
-            int mid = l+((r-l)/2);
-            if(ispossible(nums,threshold,mid)){
-                ans = mid;
-                r = mid - 1;
-            }
-            else l = mid + 1;
+            dividor = dividor + ceil((double)num / mid);
+
         }
-        return ans;
+        return dividor <= threshold;
+    } 
+    int smallestDivisor(vector<int>& nums, int threshold) {
+        int start  = 1;
+        int end = *max_element(nums.begin(),nums.end());
+        int div = -1;
+        while(start <= end){
+            int mid = (start+end)/2;
+            if(Possible(nums, threshold, mid)){
+                div = mid;
+                end = mid-1;
+            }
+            else start = mid+1;
+            
+        }
+        return div;
     }
 };

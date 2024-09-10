d92611c51ad98410d733ca168bc49214b4e42761
Pushpender Rajput <85430197+pushpenderrajput@users.noreply.github.com>
2024-09-10 10:05:54 +0530

    Time: 4 ms (75.91%), Space: 13.1 MB (38.82%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -14,11 +14,15 @@ public:
         if(head == NULL || head->next == NULL){
             return head;
         }
-        ListNode* newHead = reverseList(head->next);
-        ListNode* front = head->next;
-        front->next = head;
-        head->next = NULL;
-        return newHead;
+        ListNode* temp = head;
+        ListNode* prev = NULL;
+        while(temp != NULL){
+            ListNode* front = temp->next;
+            temp->next = prev;
+            prev = temp;
+            temp = front;
+        }
+        return prev;
         
     }
 };

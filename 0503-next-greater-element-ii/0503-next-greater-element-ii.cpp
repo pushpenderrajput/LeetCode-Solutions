0cac827ff8768522344da63f15e00d83c5094775
Pushpender Rajput <85430197+pushpenderrajput@users.noreply.github.com>
2024-11-09 22:19:35 +0530

    Time: 4 ms (74.3%), Space: 26.9 MB (29.51%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -3,14 +3,15 @@ public:
     vector<int> nextGreaterElements(vector<int>& nums) {
         int N = nums.size();
         vector<int> nge(N,-1);
-        for(int i = 0; i<N; i++){
-            for(int j = i+1; j < i+N; j++){
-                int ind = j % N;
-                if(nums[ind] > nums[i]){
-                    nge[i] = nums[ind];
-                    break;
-                }
+        stack<int> st;
+        for(int i = N*2-1; i>= 0; i--){
+            while(!st.empty() && st.top() <= nums[i%N]){
+                st.pop();
             }
+            if(i<N){
+                nge[i] = st.empty()? -1 : st.top();
+            }
+            st.push(nums[i%N]);
         }
         return nge;
         

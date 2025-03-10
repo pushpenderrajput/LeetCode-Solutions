5e0a2b0e7edadcf5ce15d87eb6830b73c1f00c88
Pushpender Rajput <85430197+pushpenderrajput@users.noreply.github.com>
2025-03-11 01:07:25 +0530

    Time: 0 ms (100%), Space: 11.7 MB (6.85%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,34 +1,24 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        // int red = 0;
-        // int blue = nums.size()-1;
-        // int white = red+1;
-        // while(white <= blue){
-        //     if(nums[white] == 2){
-        //         swap(nums[white],nums[blue]);
-        //         blue--;
-        //         white++;
+        int white = 0;
+        int red = 0;
+        int blue = nums.size()-1;
+        while(white<=blue){
+            if(nums[white] == 2){
+                swap(nums[white],nums[blue]);
+                blue--;
 
-        //     }
-        //     else if(nums[white] == 0){
-        //         swap(nums[red],nums[white]);
-        //             red++;
-                    
-                
-        //     }
-        //     else if(red == white){white++;}
-        //     else if(nums[white] == 1){
-        //         white++;
-        //     }
-        // }
-        for(int i = 0; i<nums.size();i++){
-            for(int j = i+1; j<nums.size(); j++){
-                if(nums[i]>=nums[j]){
-                    swap(nums[i],nums[j]);
-                }
             }
-        }
+            else if(nums[white] == 0){
+                swap(nums[white],nums[red]);
+                red++;
+                white++;
+            }
+            else if(nums[white] == 1){
+                white++;
+            }
+        }        
 
         
     }

f678a90865ec40d17c26af6a8a92b4fb97891eac
Pushpender Rajput <85430197+pushpenderrajput@users.noreply.github.com>
2025-03-18 16:16:18 +0530

    Time: 0 ms (100%), Space: 19.7 MB (84.42%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,35 +1,45 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int cnt1 = 0;
-        int cnt2 = 0;
-        int elm1 = INT_MIN;
-        int elm2 = INT_MIN;
-        for(auto num:nums){
-            if(cnt1 == 0 && num != elm2){
-                cnt1 = 1;
-                elm1 = num;
-            }
-            else if(cnt2 == 0 && num != elm1){
-                cnt2 = 1;
-                elm2 = num;
-            }
-            else if(elm1 == num ) cnt1++;
-            
-            else if(elm2 == num ) cnt2++;
-            else cnt1--,cnt2--;
-        }
-        vector<int> ans;
-        cnt1 = 0, cnt2 = 0;
-        for(auto num:nums){
-            if(num == elm1) cnt1++;
-            else if(num == elm2) cnt2++;
-        }
         int n = nums.size();
-        int mini = int(n/3)+1;
-        if(cnt1 >= mini) ans.push_back(elm1);
-        if(cnt2 >= mini) ans.push_back(elm2);
-        sort(ans.begin(),ans.end());
+        int candidate1 = -1;
+        int candidate2 = -1;
+        int count1 = 0;
+        int count2 = 0;
+        vector<int> ans;
+        for(int i = 0; i<n;i++){
+            if(nums[i] == candidate1){
+                count1++;
+            }else if(nums[i] == candidate2){
+                count2++;
+            }else if(count1 == 0){
+                candidate1 = nums[i];
+                count1 = 1;
+            }else if(count2 == 0){
+                candidate2 = nums[i];
+                count2 = 1;
+            }
+            else{
+                count1--;
+                count2--;
+            }
+            
+        }
+        count1 = count2 = 0;
+        for(int num:nums){
+            if(num == candidate1){
+                count1++;
+            }
+            else if(num == candidate2){
+                count2++;
+            }
+        }
+        if(count1 > n/3){
+            ans.push_back(candidate1);
+        }
+        if(count2 > n/3){
+            ans.push_back(candidate2);
+        }
         return ans;
         
     }

10a0d6947caadace1429ce07112ee72f0460b2cd
Pushpender Rajput <85430197+pushpenderrajput@users.noreply.github.com>
2025-05-05 23:17:17 +0530

    Time: 1 ms (5.51%), Space: 8.6 MB (71.71%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
-    double calculatePow(double x, long n){
-        if(n == 1)
-        return x;
-        if(n == 0) return 1;
+    double calculate(double x,long n){
+        if(n==1){
+            return x;
+        }
+        if(n == 0){
+            return 1;
+        }
         if(n%4 == 0){
-            return calculatePow(x*x,n/4) * calculatePow(x*x, n/4);
+            return calculate(x*x,n/4) * calculate(x*x,n/4);
         }
         if(n%2 == 0){
-            return calculatePow(x*x,n/2);
+            return calculate(x*x,n/2);
         }
         else{
-            return x * calculatePow(x,(n-1));
+            return x*calculate(x,n-1);
         }
+
     }
     double myPow(double x, int n) {
-        double ans = 1;
         long N;
         if(n<0){
-            N = -(long)n;
-            return 1.0 / calculatePow(x,N);
+            N=-(long)n;
+            return 1.0/calculate(x,N);
         }
-        return calculatePow(x,n);
-        
-
+        return calculate(x,n);
         
     }
 };
